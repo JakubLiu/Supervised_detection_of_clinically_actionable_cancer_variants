64b2a07e03313405c55c141c628fcf8de323c612
Jakub Liu <113781664+JakubLiu@users.noreply.github.com>
2026-02-22 00:28:46 +0100

    Update Run_Trottellumme.sh

--- BAYESIAN_PRIOR/IMPLEMENTATION/Run_Trottellumme.sh
+++ BAYESIAN_PRIOR/IMPLEMENTATION/Run_Trottellumme.sh
@@ -2,9 +2,9 @@
 
 # Rscript Trottellumme.R 'tumor.bam' 'bamlist.txt' '7' 100 100 'A' 'G' 0.01 0.5 0.00001 'output.txt'
 
-tumor_bam='/data/cephfs-1/home/users/jali13_c/work/BAYESIAN_PRIOR/TUNE_PRIOR/STEP3_SIMULATE_SNV/TRAIN/MUTATED/inserted_SNV/bwa.BIH_165-T1-DNA1-WES1.mutated.sorted.bam'
-normal_validation_bam='/data/cephfs-1/home/users/jali13_c/work/BAYESIAN_PRIOR/T-CELL-PROJECT-DATA/T_CELL-NEW/BIH_212-N1-DNA1-WES1/2021-02-16/out/bwa.BIH_212-N1-DNA1-WES1.bam'
-negative_control_bamlist='/data/cephfs-1/home/users/jali13_c/work/BAYESIAN_PRIOR/TUNE_PRIOR/STEP3_SIMULATE_SNV/all_normals.txt'
+tumor_bam='T1-DNA1-WES1.mutated.sorted.bam'
+normal_validation_bam='N1-DNA1-WES1.bam'
+negative_control_bamlist='all_normals.txt'
 chrom='7'
 start=140453136
 stop=140453136
