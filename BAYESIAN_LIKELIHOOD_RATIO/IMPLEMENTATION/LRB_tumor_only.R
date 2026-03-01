ffe0249c074f1cb11e94fe70147c0203fde02a2b
Jakub Liu <jakub-jozef.liu@charite.de>
2026-03-01 20:10:34 +0100

    add LRB tumor only code

--- BAYESIAN_LIKELIHOOD_RATIO/IMPLEMENTATION/LRB_tumor_only.R
+++ BAYESIAN_LIKELIHOOD_RATIO/IMPLEMENTATION/LRB_tumor_only.R
@@ -1,13 +1,24 @@
 suppressPackageStartupMessages(library(Rsamtools))
 suppressPackageStartupMessages(library(Biostrings))
 suppressPackageStartupMessages(library(stats))
-
-library(Rsamtools)
-library(Biostrings)
-library(stats)
+suppressPackageStartupMessages(library(GenomicAlignments))
+suppressPackageStartupMessages(library(GenomicRanges))
+suppressPackageStartupMessages(library(data.table))
 
 make_pileups <- function(tumor_bam_path, negative_control_bamlist, CHROM, START, END, write_files = FALSE){
 
+    print(paste0(
+        'tumor bam: ',
+        tumor_bam_path
+    ))
+
+    print(paste0(
+        'negative control bamlist: ',
+        negative_control_bamlist
+    ))
+
+
+    print('setting pileup parameters...')
     CHROM <- as.character(CHROM)
     START <- as.numeric(START)
     END <- as.numeric(END)
@@ -16,17 +27,21 @@ make_pileups <- function(tumor_bam_path, negative_control_bamlist, CHROM, START,
     actionable_region <- GRanges(CHROM, IRanges(start = START, end = END))
     scan_bam_param <- ScanBamParam(which = actionable_region)
     pileup_param <- PileupParam(distinguish_strands = TRUE, distinguish_nucleotides = TRUE)
+    print('setting pileup parameters done.')
 
 
     # pileup on the target tumor sample
+    print('creating pileups...')
     target_tumor_pileup <- pileup(file = tumor_bam_path, index = paste0(tumor_bam_path, ".bai"), scanBamParam = scan_bam_param, pileupParam = pileup_param)
     target_tumor_pileup_R1 <- target_tumor_pileup[target_tumor_pileup$strand == '+',]  # the pileup in the target tumor sample, only the forward reads
     target_tumor_pileup_R2 <- target_tumor_pileup[target_tumor_pileup$strand == '-',]  # the pileup in the target tumor sample, only the backward reads
+    print('creating pileups done.')
 
     # pileup in the negative control cohort
+    print('creating the negative control cohort pileup...')
     negative_control_list <- readLines(negative_control_bamlist)
-    negative_control_pileup_R1 <- rep(-9,6)
-    negative_control_pileup_R2 <- rep(-9,6)
+    negative_control_pileup_R1 <- NULL
+    negative_control_pileup_R2 <- NULL
     
     for(i in 1:length(negative_control_list)){
 
@@ -42,6 +57,7 @@ make_pileups <- function(tumor_bam_path, negative_control_bamlist, CHROM, START,
 
     negative_control_pileup_R1 <- negative_control_pileup_R1[2:nrow(negative_control_pileup_R1),]
     negative_control_pileup_R2 <- negative_control_pileup_R2[2:nrow(negative_control_pileup_R2),]
+    print('creating the negative control cohort pileup done.')
 
     if(write_files == TRUE){
 
@@ -53,12 +69,13 @@ make_pileups <- function(tumor_bam_path, negative_control_bamlist, CHROM, START,
         write.csv(negative_control_pileup_R2, paste0(current_time, '_N.R2.csv'), row.names = F)
     }
 
-    list(
+    return(list(
         'target_tumor_pileup_R1' = target_tumor_pileup_R1,
         'target_tumor_pileup_R2' = target_tumor_pileup_R2,
         'negative_control_pileup_R1' = negative_control_pileup_R1,
         'negative_control_pileup_R2' = negative_control_pileup_R2
-    )
+    ))
+    print('all done.')
 
     
 }
@@ -72,7 +89,7 @@ estimate_and_count <- function(TR1,   # pileup of the tumor sample in the forwar
                                alternative_allele,  # the alternative allele as is in the actionable variant
                                write_file = FALSE  # optionally write the estimates to a file
                                ){
-
+    print('estimating parameters from pileups...')
     alternative_allele <- as.character(alternative_allele)
     # the counts _______________________________________________________________________________________________
     # the n_i 's in the paper
@@ -80,6 +97,10 @@ estimate_and_count <- function(TR1,   # pileup of the tumor sample in the forwar
     covTR2 <- sum(TR2$count)  # the coverage in the reverse direction in the tumor sample
     covNR1 <- sum(NR1$count)  # the total coverage (sum over all samples) in the negative cohort in the forward direction
     covNR2 <- sum(NR2$count)  # the total coverage (sum over all samples) in the negative cohort in the reverse direction
+
+    if(covTR1 == 0 || covTR2 == 0 || covNR1 == 0 || covNR2 == 0){
+      stop('Error. The coverage in at least one of the directions is either the target sample or the negative control cohort is zero.')
+    }
 
     # the X_i 's in the paper
     countTR1 <- sum(TR1$count[TR1$nucleotide == alternative_allele]) # count of the alternative allele in the tumor in the forward direction
@@ -117,7 +138,8 @@ estimate_and_count <- function(TR1,   # pileup of the tumor sample in the forwar
                   )
     }
 
-    estimates
+    return(estimates)
+    print('estimating parameters from pileups done.')
 
 }
 
@@ -137,6 +159,7 @@ calc_posterior <- function(countTR1, countTR2,  # the allele counts
       # mu0 ---> the observed tumor VAF
       # vi ---> the estimated background error rate
       # BF = P(data|H1)/P(data|H0), where data are the observed allele counts 
+      print('calculating bayes factor...')
 
       prob_H0_R1 <- dbinom(x = countTR1, size = covTR1, prob = vi_R1, log = TRUE) # probability of observing the tumor VAF given the tumor coverage and the background error rate (forward reads)
       prob_H0_R2 <- dbinom(x = countTR2, size = covTR2, prob = vi_R2, log = TRUE) # probability of observing the tumor VAF given the tumor coverage and the background error rate (reverse reads)
@@ -145,9 +168,13 @@ calc_posterior <- function(countTR1, countTR2,  # the allele counts
 
       logBF <- (prob_H1_R1 + prob_H1_R2) - (prob_H0_R1 + prob_H0_R2)
       BF <- exp(logBF)
+      print('calculating bayes factor done.')
+      print('calculating posterior...')
       posterior <- BF * prior / (BF * prior + (1 - prior)) # based on the Bayes factor get the posterior
+      print('calculating posterior done.')
 
-      list("prior" = prior, "posterior" = posterior, "bayes_factor" = BF)
+      output <- list("prior" = prior, "posterior" = posterior, "bayes_factor" = BF)
+      return(output)
 }
 
 
@@ -156,6 +183,8 @@ call_variant <- function(sample, posterior_cutoff, prior, posterior, bayes_facto
                         covTR1, covTR2, countTR1, countTR2, covNR1, covNR2, countNR1, countNR2,
                         vi_R1, vi_R2, mu0_R1, mu0_R2, pseudocount, output_file){
                         
+                        print('calling variants...')
+
                         names <- c('sample','chrom','start','stop','ref','alt',
                                    'decision', 'posterior', 'posterior_cutoff', 'prior', 'bayes_factor',
                                    'coverage_tumor_R1', 'coverage_tumor_R2',
@@ -179,8 +208,168 @@ call_variant <- function(sample, posterior_cutoff, prior, posterior, bayes_facto
                         write.csv(output, output_file, row.names = FALSE)
 
                         return(output)
+                        print('done.')
 
                          }
+
+
+
+annotate_reads <- function(bam, chrom, pos, alt, output_file_name = 0){
+    
+    chrom <- as.character(chrom)
+    pos <- as.numeric(pos)
+    alt <- as.character(alt)
+
+    param <- ScanBamParam(which = GRanges(chrom, IRanges(pos, pos)), what = c("qname", "seq", "qual", "mapq"))
+    gal <- readGAlignments(bam, param = param)
+    locus <- GRanges(chrom, IRanges(pos, pos))
+    names(gal) <- mcols(gal)$qname
+    mapped <- mapToAlignments(locus, gal) # fetch the reads that map to the locus
+    seqs <- mcols(gal)$seq # the sequences of the reads
+    alt_read_mask <- mapply(function(s, p) {as.character(subseq(s, p, p))}, seqs, start(mapped)) == alt
+    alt_reads <- gal[alt_read_mask] # extract only the alternative reads
+    alt_reads_seq <- as.character(mcols(alt_reads)$seq) # the sequences of the full reads
+    mapq <- mcols(alt_reads)$mapq  # extract mapping qualities
+    position_in_read <- start(mapped)[alt_read_mask]  # position in the read
+    read_length <- width(mcols(alt_reads)$seq)  # length of the read
+    strand <- as.character(strand(alt_reads))  # strand "+" = R1, "-" = R2
+    basecallig_qualities <- mapply(function(q, p) {as.integer(subseq(q, p, p))}, mcols(alt_reads)$qual, position_in_read)  # as.integer automatically changes the ASCII Phred score to the numerical score
+    
+    output <- data.frame(cbind(alt_reads_seq, mapq, basecallig_qualities, read_length, position_in_read, strand))
+    colnames(output) <- c('full_read_sequence', 'mapping_quality', 'basecalling_quality', 'read_length',
+                          'position_within_the_read', 'strand')
+
+    if(output_file_name != 0){
+        output_file_name <- as.character(output_file_name)
+        write.csv(output, output_file_name, row.names = FALSE)
+    }
+
+    return(output)
+}
+
+
+raise_warnings <- function(report_table, mapQ_thresh, baseQ_thresh, output_file_name){
+
+        raised_warnings <- c()
+        n_alt <- nrow(report_table)
+
+        strand_bias <- FALSE
+        # check if all alternative reads are in one direction
+        if(length(unique(report_table$strand)) == 1){
+            strand_bias <- TRUE
+        }
+
+        low_mapQ_count <- 0
+        low_baseQ_count <- 0
+        end_of_read_count <- 0
+
+        for(i in 1:nrow(report_table)){
+
+            current_read <- report_table[i,]
+
+            # catch low mapping quality reads
+            if(current_read$mapping_quality < mapQ_thresh){low_mapQ_count <- low_mapQ_count + 1}
+
+            # catch low basecalling quality reads
+            if(current_read$basecalling_quality < baseQ_thresh){low_baseQ_count <- low_baseQ_count + 1}
+            
+            # catch bases that are in the last quarter of their respective reads
+            if(current_read$position_within_the_read > as.numeric(current_read$read_length) * 0.75){
+                end_of_read_count <- end_of_read_count + 1
+            }
+        }
+
+        warnings <- list(
+                        'strand_bias_filter_failed' = strand_bias,
+                        'num_reads_failed_mapQ_filter' = low_mapQ_count,
+                        'num_reads_failed_baseQ_filter' = low_baseQ_count,
+                        'num_reads_failed_late_cycle_filter' = end_of_read_count
+                        )
+
+        write.csv(data.frame(warnings), output_file_name, row.names = FALSE)
+
+}
+
+
+raise_context_warnings <- function(reference_genome, chr, pos, alt, padding_upstream = 10, padding_downstream = 10,
+                                    GC_content_min = 0.35, # lower bound of the 'normal' GC content [2]
+                                    GC_content_max = 0.60, # upper bound for the 'normal' GC content [2]
+                                    min_upstream_homopolymer_length = 5,  # look at the explanation below in the code
+                                    output_file_name){  
+
+    chr <- as.character(chr)
+    pos <- as.numeric(pos)
+    alt <- as.character(alt)
+    padding_upstream <- as.numeric(padding_upstream)
+    padding_downstream <- as.numeric(padding_downstream)
+    refgen <- FaFile(reference_genome) # the fasta must also have the index (.fa.fai)
+    open(refgen)
+    context_window <- GRanges(seqnames = chr, ranges = IRanges(start = pos - padding_upstream, end = pos + padding_downstream))
+    context_window_GC <- GRanges(seqnames = chr, ranges = IRanges(start = pos - 50000, end = pos + 50000)) # extract a 100Kb fragment for computing the GC content [2]
+    context <- as.character(getSeq(refgen, context_window))  # get the genomic context
+    context_GC <- as.character(getSeq(refgen, context_window_GC)) # the 10Kb genomic context for the GC content calculation
+    close(refgen)
+    upstream_context <- substr(context, 1, padding_upstream) # the genomic context upstream of the target locus
+    downstream_context <- substr(context, padding_upstream + 2, nchar(context)) # the genomic context downstream of the target locus
+
+
+    # search for error linked motifs upstream of the target locus [1]
+    GG_motif_upstream <- FALSE
+    if(grepl(("GG"), upstream_context)){
+        GG_motif_upstream <- TRUE
+    }
+    GGC_motif_upstream <- FALSE
+    if(grepl(("GGC"), upstream_context)){
+        GGC_motif_upstream <- TRUE
+    }
+    GGT_motif_upstream <- FALSE
+    if(grepl(("GGT"), upstream_context)){
+        GGT_motif_upstream <- TRUE
+    }
+
+    GC_content <- sum(strsplit(context_GC, "")[[1]] %in% c("G", "C"))/nchar(context_GC)
+    abnormal_GC_content <- FALSE
+    if((GC_content < GC_content_min) | (GC_content > GC_content_max)){
+        abnormal_GC_content <- TRUE
+    }
+    
+    # check if a homopolymer of the same base as the alternative, terminates exactly at the alternative [3]
+    #
+    # example of a positive case:
+    # AAAAAAAAAAAATGCTACC
+    #            ^
+    #            |
+    #       locus of interest
+    #
+    potential_homopolymer_upstream_region <- substr(upstream_context, nchar(upstream_context)-min_upstream_homopolymer_length, nchar(upstream_context))
+    homopolymer <- strrep(alt, min_upstream_homopolymer_length)
+    homopolymer_upstream <- FALSE
+    if(potential_homopolymer_upstream_region == homopolymer){
+        if(substr(downstream_context,1,1) != alt){
+            homopolymer_upstream <- TRUE
+        }
+    }
+
+    result <- list(
+        'GG_motif_upstream_present' = GG_motif_upstream,
+        'GGT_motif_upstream_present' = GGT_motif_upstream,
+        'GGC_motif_upstream_present' = GGC_motif_upstream,
+        'abnormal_GC_content_warning' = abnormal_GC_content,
+        'preceeding_homopolymer_present' = homopolymer_upstream
+    )
+
+    write.csv(data.frame(result), output_file_name, row.names = FALSE)
+}
+
+# [1] Meacham, F., Boffelli, D., Dhahbi, J., Martin, D. I., Singer, M., & Pachter, L. (2011). Identification and correction 
+#     of systematic error in high-throughput sequence data. BMC bioinformatics, 12, 451. 
+#     https://doi.org/10.1186/1471-2105-12-451
+#
+# [2] https://en.wikipedia.org/wiki/GC-content#Among-genome_variation
+#
+# [3] Stoler N, Nekrutenko A. Sequencing error profiles of Illumina sequencing instruments. NAR Genom Bioinform. 
+#     2021 Mar 27;3(1):lqab019. doi: 10.1093/nargab/lqab019. PMID: 33817639; PMCID: PMC8002175.
+
 
 
 #______________________________________________________________________________________________________________
@@ -198,6 +387,13 @@ prior <- as.numeric(args[8])
 posterior_cutoff <- as.numeric(args[9])
 pseudocount <- as.numeric(args[10])
 output_file <- as.character(args[11])
+mapQ_thresh <- as.numeric(args[12])
+baseQ_thresh <- as.numeric(args[13])
+output_annotation_file <- as.character(args[14])
+reference_genome <- as.character(args[15])
+padding_upstream <- as.numeric(args[16])
+padding_downstream <- as.numeric(args[17])
+output_context_warning_file <- as.character(args[18])
 
 
 
@@ -254,12 +450,31 @@ output <- call_variant(sample = tumor_bam_path,
                        output_file = output_file)
 
 
-print(output)
+
+read_annotation <- annotate_reads(
+                bam = tumor_bam_path,
+                chrom = chromosome,
+                pos = start,
+                alt = alt)
 
 
-# usage:
+read_annotation <- raise_warnings(
+                                    report_table = read_annotation,
+                                    mapQ_thresh = mapQ_thresh,
+                                    baseQ_thresh = baseQ_thresh,
+                                    output_file_name = output_annotation_file
+)
 
-# Rscript Trottellumme.R 'tumor.bam' 'bamlist.txt' '7' 100 100 'A' 'G' 0.01 0.5 0.00001 'output.txt'
 
+raise_context_warnings(
+                            reference_genome = reference_genome,
+                            chr = chromosome,
+                            alt = alt,
+                            pos = start,
+                            padding_downstream = padding_downstream,
+                            padding_upstream = padding_upstream,
+                            output_file_name = output_context_warning_file
+)
 
+# _____________________________________________________________________________________________________________________________________
 
