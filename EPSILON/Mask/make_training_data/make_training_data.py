be05d6bfed0e795e87485cafd8bb2df984bee683
Jakub Liu <jakub-jozef.liu@charite.de>
2026-06-23 13:19:06 +0200

    update training data generation script

--- EPSILON/Mask/make_training_data/make_training_data.py
+++ EPSILON/Mask/make_training_data/make_training_data.py
@@ -28,22 +28,42 @@ def make_training_data(filtered_data, fname_X, fname_alt_counts, fname_ref_count
 
     for i in range(windowsize_left+1, data.shape[0]-(windowsize_left+1)):
 
-        print(str(i/data.shape[0]*100)+'%')
-        
-        alt_counts_list.append(int(data.loc[i,'general_alt_counts']))
-        ref_counts_list.append(int(data.loc[i,'ref_counts']))
+        chrom = str(data.loc[i,'chrom'])
+        pos = int(data.loc[i,'pos'])
+
+        alt_counts_list.append([chrom, pos, int(data.loc[i,'general_alt_counts'])])
+        ref_counts_list.append([chrom, pos, int(data.loc[i,'ref_counts'])])
+       
         
         # the predictors upstream and downstream of the given locus
         context_upstream = data.iloc[(i-windowsize_left):i,5:]
         context_downstream = data.iloc[(i+1):(i+windowsize_right),5:]
         x = np.concatenate([context_upstream.values.flatten(), context_downstream.values.flatten()])
-        X_list.append(x)
+        X_list.append([chrom, pos, *x])
 
 
     X = pd.DataFrame(X_list)
-    alt_counts_list = pd.Series(alt_counts_list)
-    ref_counts_list = pd.Series(ref_counts_list)
+    alt_counts_list = pd.DataFrame(alt_counts_list)
+    ref_counts_list = pd.DataFrame(ref_counts_list)
 
     X.to_csv(fname_X, index = False)
     alt_counts_list.to_csv(fname_alt_counts, index = False)
     ref_counts_list.to_csv(fname_ref_counts, index = False)
+
+
+
+
+#data = pd.read_csv('raw_data.anonym.csv')
+
+#print(data.head())
+
+make_training_data(
+			filtered_data = 'raw_data.anonym.csv',
+			fname_X = 'output2.csv',
+			fname_alt_counts = 'alt_counts2.txt',
+			fname_ref_counts = 'ref_counts2.txt',
+			
+)
+
+
+print('done.')
