02a175f3ac048da787dcf58b0df352aa7d837d64
Jakub Liu <113781664+JakubLiu@users.noreply.github.com>
2026-08-25 15:38:38 +0200

    Update subsample_loci.R

--- EPSILON/EpsilonTO/make_data/make_training_data/subsample_loci.R
+++ EPSILON/EpsilonTO/make_data/make_training_data/subsample_loci.R
@@ -4,7 +4,7 @@ args <- commandArgs(trailingOnly = TRUE)
 input_file <- as.character(args[1])
 output_file <- as.character(args[2])
 subsampling_strength <- as.numeric(args[3])
-data <- fread('merged.processed.mpileup.txt')
+data <- fread(input_file)
 nrows <- nrow(data)*subsampling_strength
 idx <- sample(1:nrow(data), nrows, replace = FALSE)
 data_subsampled <- data[idx,]
